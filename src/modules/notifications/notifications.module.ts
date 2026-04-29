4a6c56ddbe78130d6a64f9eac05a906c5a16b66c
Alexisvip100 <alexisultreras01@gmail.com>
2026-04-29 17:37:02 -0600

    fix: add notification settings

--- src/modules/notifications/notifications.module.ts
+++ src/modules/notifications/notifications.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
-
+import { NotificationsCronService } from './notifications-cron.service';
 import { NotificationsController } from './notifications.controller';
+import { TasksModule } from '../tasks/tasks.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  providers: [NotificationsService],
+  imports: [TasksModule, UsersModule],
+  providers: [NotificationsCronService, NotificationsService],
   controllers: [NotificationsController],
   exports: [NotificationsService],
 })
